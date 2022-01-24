379b684c03111ca7f88a50ca8776880094631ff0
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-01-23 21:29:58 -0300

    teste

--- ecm251-2021-lab3/aula_flutter/aula1/lib/main.dart
+++ ecm251-2021-lab3/aula_flutter/aula1/lib/main.dart
@@ -1,51 +1,65 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Anime extends StatefulWidget {
+  const Anime({Key? key}) : super(key: key);
+
+  @override
+  _AnimeState createState() => _AnimeState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _AnimeState extends State<Anime> {
+  var animeid, title, description, score, coverPhoto, genre;
+  @override
+  void initState() {
+    super.initState();
+    attInterface();
+  }
 
-  // This widget is the root of your application.
+  var list;
+  void attInterface() async {
+    final respose = await http.get(Uri.parse('http://localhost:8080/anime/2'));
+    list = await http.get(Uri.parse('http://localhost:8080/anime'));
+    list = jsonDecode(list.body);
+    print(list);
+    setState(() {
+      if (respose.body == null) {
+      } else {
+        var jsonData = json.decode(respose.body);
+        animeid = jsonData['anime_id'].toString();
+        title = jsonData['title'].toString();
+        description = jsonData['description'].toString();
+        score = jsonData['score'].toString();
+        coverPhoto = jsonData['coverPhoto'].toString();
+        genre = jsonData['genre'].toString();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Text(title),
+            Text(description),
+            Text(score),
+            Text(genre),
+            Image.network(coverPhoto),
+            for (var i in list) Image.network(i['coverPhoto'].toString()),
+          ],
+        ),
       ),
-      home: const MinhaLisatEstatica(Key? : key) : super(key:key););
+    );
   }
 }
-  class MinhaLisatEstatica extends StatelessWidget {
-    const MinhaLisatEstatica({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Lista Estatica'),
-        ),
-        body: ListView(
-          children: const <Widget>[
-            ListTile(
-              leading: Icon(Icons.map),
-              title: Text('Mapa'),
-            ),
-            
-          ]
-      ));
-    }
-  }
-
-
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Anime(),
+  ));
+}
